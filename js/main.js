2e303830eba8bcba81d654f03a0248ca671036e6
akknvh3331 <akknvh3331.@gmail.com>
2022-01-13 19:51:21 +0300

    perviy commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.header__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./images/Vector.svg" alt="button"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./images/ee.svg" alt="top"></button>',
+        vertical:true
+    });
+});
